2bde6f30b02e4057bbe9199c7e526e9beeffd312
Kobbysaah <102618159+Kobbysaah@users.noreply.github.com>
2024-05-16 11:54:46 +0000

    Update arrayManipulation.js
    added a new function that accept string and numbers into array and produce an output of lower casing or upper casing

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -13,7 +13,33 @@ function processArray(inputArray) {
   return processedArray;
 }
 
+function formatArrayStrings(stringsArray, numbersArray) {
+    if (stringsArray.length !== numbersArray.length) {
+        console.error("Arrays must have the same length.");
+        return;
+    }
+
+    for (var i = 0; i < stringsArray.length; i++) {
+        var str = stringsArray[i];
+        var num = numbersArray[i];
+
+        if (num % 2 === 0) {
+            stringsArray[i] = str.toUpperCase();
+        } else {
+            stringsArray[i] = str.toLowerCase();
+        }
+    }
+    return stringsArray;
+}
+
 // Example usage:
+//1
 var input = [1, 2, 3, 4, 5];
 var output = processArray(input);
 console.log(output);
+
+//2
+var strings = ["Hello", "World", "JavaScript", "Obiaabiowa"	];
+var numbers = [1, 2, 3,4];
+var formattedStrings = formatArrayStrings(strings, numbers);
+console.log(formattedStrings);
